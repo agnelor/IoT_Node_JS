37f779e14ed7391792676ab3785fac7c7c608621
Ramki <ramkicse@gmail.com>
2017-01-27 16:32:27 +0530

    add package and merge agnelo code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var fs = require("fs");
 var mqtt = require('mqtt')
 var express = require("express");
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
 
 
-console.log("Starting");
 var config = JSON.parse(fs.readFileSync("config.json"));
 
 var host = config.host;
@@ -13,40 +14,36 @@ var mqtt_server=config.mqtt_server;
 var client = mqtt.connect(mqtt_server)
 var app = express();
 
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static("./app"));
 
-app.get('/', function (request, response) {
+// use morgan to log requests to the console
+app.use(morgan('dev'));
 
+
+app.get('/', function (request, response) {
+	
     // Note: __dirname is directory that contains the JavaScript source code. 
 	response.sendFile(__dirname + '/app/controller.html')
 })
 
 app.get("/light/:text", function(request,response) {
-	
-
-	
-	client.publish('iot', 'Home/Hall/Light/' + request.params.text)	
-	
-	response.end();
-	
+	client.publish('iot', 'Home/Hall/Light/' + request.params.text)		
+	response.end();	
 });
 
-app.get("/fan/:text", function(request,response) {
-	
-	
-	client.publish('iot', 'Home/Hall/Fan/' + request.params.text)	
-	
-	response.end();
-	
+app.get("/fan/:text", function(request,response) {	
+	client.publish('iot', 'Home/Hall/Fan/' + request.params.text)		
+	response.end();	
 });
 
-app.get("/alarm/:text", function(request,response) {
-	
-	
-	client.publish('iot', 'Home/Hall/Alarm/' + request.params.text)	
-	
-	response.end();
-	
+app.get("/alarm/:text", function(request,response) {	
+	client.publish('iot', 'Home/Hall/Alarm/' + request.params.text)		
+	response.end();	
 });
 
 app.listen(port,host);
